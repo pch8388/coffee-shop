3eefdeb3c1752c6f522a8794127acf1f26e314cb
sc <pch838811@gmail.com>
2022-03-04 00:24:08 +0900

    spring cloud gateway init

---
+++ coffee-gateway/src/main/java/me/study/coffeegateway/CoffeeGatewayApplication.java
@@ -0,0 +1,13 @@
+package me.study.coffeegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeGatewayApplication.class, args);
+    }
+
+}
---
+++ coffee-gateway/src/test/java/me/study/coffeegateway/CoffeeGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.coffeegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
